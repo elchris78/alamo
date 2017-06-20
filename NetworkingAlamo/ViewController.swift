1c39a7bcf422dfe76410707bdd5605a11d3db86a
elchris78 <el_chrispin@hotmail.com>
2017-06-20 13:52:22 -0500

    Mi primer commit de nuevo

--- NetworkingAlamo/ViewController.swift
+++ NetworkingAlamo/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var probandoGit2: Bool!
+    var agregandoNuevaVariable = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
